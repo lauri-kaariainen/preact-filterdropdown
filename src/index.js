152266ea7e9335cc1470b3fade6081e2be865d95
Lauri Kääriäinen <4522622+lauri-kaariainen@users.noreply.github.com>
2020-04-26 18:09:45 +0300

    cleaning commented out code

--- src/index.js
+++ src/index.js
@@ -8,59 +8,6 @@ import {
 } from "preact/hooks";
 import { FilterDropdown } from "./FilterDropdown.js";
 
-// import "./style/filterdropdown.scss";
-
-// const FilterDropDown = ({ list, onselect }) => {
-//   const [inputStr, setInputStr] = useState("");
-
-//   const [dropDownHidden, setDropDownHidden] = useState(true);
-
-//   const closeDropdown = () => {
-//     setDropDownHidden(true);
-//     // dropdown.classList.remove("open");
-//   };
-
-//   const onFilterInput = str => setInputStr(str);
-
-//   const visibleList = list.filter(item =>
-//     item.toLowerCase().includes(inputStr.toLowerCase())
-//   );
-
-// document.addEventListener("click", evt => {
-//   const isDropdown = dropdown.contains(evt.target);
-//   const isInput = inputField.contains(evt.target);
-//   if (!isDropdown && !isInput) {
-//     dropdown.classList.remove("open");
-//   }
-// });
-
-// Similar to componentDidMount and componentDidUpdate:
-
-//   return (
-//     <div class="dropdown_container">
-//       <input
-//         className={"chosen-value"}
-//         oninput={evt => onFilterInput(evt.target.value)}
-//         type="text"
-//         value={inputStr}
-//         onclick={_ => setDropDownHidden(!dropDownHidden)}
-//         placeholder="Type to filter"
-//       />
-//       <ul className={"value-list" + (!dropDownHidden ? " open" : "")}>
-//         {visibleList.map(el => (
-//           <li
-//             className={"open"}
-//             key={el}
-//             onclick={_ => (setInputStr(el), setDropDownHidden(true))}
-//           >
-//             {el}
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
 function App() {
   return (
     <Fragment>
@@ -72,7 +19,6 @@ function App() {
         list={["alabama", "alaska", "arizona"]}
         onselect={evt => console.log(evt)}
       />
-      {/* <Example /> */}
     </Fragment>
   );
 }
